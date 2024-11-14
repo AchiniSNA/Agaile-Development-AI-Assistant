8a4c1537ae2c8902aae5d47fdab437a5d6eb7d01
Achini Sanjula <achini.20@cse.mrt.ac.lk>
2024-11-14 22:57:27 +0530

    app.py changed

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ import requests
 import os
 from datetime import datetime
 from dotenv import load_dotenv
+
 load_dotenv()
 
+#api_key = os.getenv("GOOGLE_API_KEY")
+#cse_id = os.getenv("CSE_ID")
 
+#genai.configure(api_key=api_key)
 #genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 # Define the Google Custom Search API method to search online for relevant results
 def search_online(query):
-    api_key = 'AIzaSyAimoBpUkJ41hTqW_cXlcb0cHJOXukUGOg'  # Replace with your Google API key
-    cse_id = '17d59fa1418364d8a'  # Replace with your Custom Search Engine ID
+    api_key = os.getenv("GOOGLE_API_KEY")
+    cse_id = os.getenv("CSE_ID")
+    #api_key = 'AIzaSyAimoBpUkJ41hTqW_cXlcb0cHJOXukUGOg'  # Replace with your Google API key
+    #cse_id = '17d59fa1418364d8a'  # Replace with your Custom Search Engine ID
     if not api_key or not cse_id:
         st.error("API key or Custom Search Engine ID is missing.")
         return []
@@ -105,7 +111,7 @@ for index, session_id in enumerate(st.session_state["chat_sessions"]):
 
 # Main chat area
 st.title("Scrum Development AI Assistant 💁")
-st.markdown("Welcome to Agile Mentor! Ask any question to get started.")
+st.markdown("Welcome to Scrum Mentor! Ask any question to get started.")
 
 # Display the current session's chat history
 current_session = st.session_state["chat_sessions"].get(st.session_state["current_session_id"], [])
